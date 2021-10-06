bc6603a2337115380a6e4fba558db2b7809465a3
Kazuma Oe <koneko0604@gmail.com>
2021-10-06 04:24:49 +0000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     vite: {
-      plugins: [WindiCSS.default()]
+      plugins: [WindiCSS()]
     }
   }
 };
