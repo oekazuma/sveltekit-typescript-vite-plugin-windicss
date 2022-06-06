5e528eeb53da173ab4bbc8200374a9e7ef0136c6
Kazuma Oe <koneko0604@gmail.com>
2022-06-06 02:54:28 +0000

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,13 +4,7 @@ module.exports = {
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['svelte3', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [
-		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3',
-			rules: { '@typescript-eslint/ban-ts-comment': 'off' }
-		}
-	],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,5 @@
 <script>
 	import 'virtual:windi.css';
-	import { browser } from '$app/env';
-	// @ts-ignore: Cannot find module 'virtual:windi-devtools' or its corresponding type declarations.
-	if (browser) import('virtual:windi-devtools');
 </script>
 
 <main>
